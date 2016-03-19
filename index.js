4d30053674a4ec27e3f33da86fff66bb7a1724dd
Kyle Kelley <rgbkrk@gmail.com>
2016-03-19 15:33:24 -0400

    ANSI to React

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('soon');
+import React from 'react';
+
+const ansiToJSON = require('ansi-to-json');
+
+function ansiJSONtoStyleBundle(ansiBundle) {
+  const style = {};
+  if(ansiBundle.bg) {
+    style.backgroundColor = `rgb(${ansiBundle.bg})`;
+  }
+  if(ansiBundle.fg) {
+    style.color = `rgb(${ansiBundle.fg})`;
+  }
+  return {
+    content: ansiBundle.content,
+    style,
+  };
+}
+
+function ansiToInlineStyle(text) {
+  return ansiToJSON(text).map(ansiJSONtoStyleBundle)
+}
+
+function inlineBundleToReact(bundle, key) {
+  return <span style={bundle.style} key={key}>{bundle.content}</span>
+}
+
+export default function Ansi(props) {
+  const { text } = props;
+  return (
+    <code>
+      {ansiToInlineStyle(text).map(inlineBundleToReact)}
+    </code>
+  )
+}
+
